8958302cad926fc9d430aa9689e752a90973536d
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-01-28 01:27:10 +0530

    Start blockchain functionality

---
+++ Node/helpers.go
@@ -0,0 +1,31 @@
+package main
+
+// Creation of a merkle tree and storage
+func makeMerkle() {
+
+}
+
+// Mining of a block
+func mineBlock() {
+
+}
+
+// Sending UTXOs
+func sendFunds() {
+
+}
+
+// Validation of a block by checking the previous hash, and all the transactions
+func validateBlock() {
+
+}
+
+// Download the blockchain when the full node starts up
+func downloadBlockchain() {
+
+}
+
+// Remove the confirmed transactions from the mempool
+func removeTxns(block Block) {
+
+}
--- Node/streams.go
+++ Node/streams.go
@@ -6,6 +6,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/network"
 )
 
+// Propagation handlers
 func broadcastMessage(stream network.Stream) {
 
 	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
@@ -13,24 +14,38 @@ func broadcastMessage(stream network.Stream) {
 
 }
 
-func broadcastTxn(stream network.Stream, txn Transaction) {
+func broadcastTxn(stream network.Stream) {
 
 	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
 	go propagateTxn(rw, stream)
 
 }
 
-func broadcastBlock(stream network.Stream, block Block) {
+func broadcastBlock(stream network.Stream) {
 
 	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
 	go propagateBlock(rw, stream)
 
 }
 
+// Messaging Stream Handlere
 func messageProtocol(stream network.Stream) {
 
 	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
 
 	go readData(rw)
 	// go writeData(rw)
+}
+
+func downloadProtocol(stream network.Stream) {
+
+}
+
+// Reciever Stream handlers
+func blockProtocol(stream network.Stream) {
+
+}
+
+func txnProtocol(stream network.Stream) {
+
 }
