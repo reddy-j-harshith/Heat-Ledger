d3420f2f900a92acd1bf1ea0de437947335430e7
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-01-30 00:42:05 +0530

    Refactors

--- Node/flags.go
+++ Node/flags.go
@@ -53,11 +53,10 @@ func ParseFlags() (Config, error) {
 	config := Config{}
 
 	// flag.StringVar(pointer, name, defaultValue, usage)
-	flag.StringVar(&config.RendezvousString, "rendezvous", "meet me here",
-		"Unique string to identify group of nodes. Share this with your friends to let them connect with you")
+	flag.StringVar(&config.RendezvousString, "rendezvous", "meet me here", "Unique string to identify group of nodes.")
 	flag.Var(&config.BootstrapPeers, "peer", "Adds a peer multiaddress to the bootstrap list")
 	flag.Var(&config.ListenAddresses, "listen", "Adds a multiaddress to the listen list")
-	flag.StringVar(&config.ProtocolID, "pid", "/chat/1.0.0", "Sets a protocol id for stream headers")
+	flag.StringVar(&config.ProtocolID, "pid", "/blockchain/1.0.0", "Sets a protocol id for stream headers")
 	flag.Parse()
 
 	if len(config.BootstrapPeers) == 0 {
