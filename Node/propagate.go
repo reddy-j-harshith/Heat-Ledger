3e2dbec658b9f156de12c5b1dbece316c71baa9e
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-01-31 16:19:04 +0530

    Propagate complete

--- Node/propagate.go
+++ Node/propagate.go
@@ -114,10 +114,179 @@ func propagateMessage(rw *bufio.ReadWriter, strm network.Stream) {
 	}
 }
 
+// Propagate the transaction to the network - Mempool
 func propagateTxn(rw *bufio.ReadWriter, strm network.Stream) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic in propagateTxn:", r)
+		}
+	}()
 
+	for {
+		line, err := rw.ReadString('\n')
+		if err != nil {
+			return
+		}
+
+		if line == "" || line == "\n" {
+			continue
+		}
+
+		var transaction Transaction
+		err = json.Unmarshal([]byte(line), &transaction)
+		if err != nil {
+			fmt.Println("Failed to parse JSON:", err)
+			continue
+		}
+
+		// Add the transaction to the mempool
+		MempoolMutex.RLock()
+		_, exists := Mempool[transaction.Txn_id]
+		MempoolMutex.RUnlock()
+
+		if !exists {
+			MempoolMutex.Lock()
+			Mempool[transaction.Txn_id] = transaction
+			MempoolMutex.Unlock()
+		} else {
+			continue
+		}
+
+		fmt.Printf("\x1b[32m> New Txn Added to Mempool\n> Txn_id: %s\n> Sent from %s\x1b[0m\n", transaction.Txn_id, strm.Conn().RemotePeer())
+
+		peerMutex.RLock()
+		peers := peerArray
+		peerMutex.RUnlock()
+
+		for _, peer := range peers {
+			if peer.ID == strm.Conn().RemotePeer() {
+				continue
+			}
+
+			stream, err := User.NewStream(context.Background(), peer.ID, protocol.ID("/blockchain/1.0.0/broadcast/transaction"))
+			if err != nil {
+				continue
+			}
+
+			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+			data, err := json.Marshal(transaction)
+			if err != nil {
+				fmt.Println("Failed to serialize transaction:", err)
+				stream.Close()
+				continue
+			}
+
+			_, err = rw.WriteString(string(data) + "\n")
+			if err != nil {
+				fmt.Println("Failed to send transaction to peer:", peer.ID, err)
+				stream.Close()
+				continue
+			}
+
+			err = rw.Flush()
+			if err != nil {
+				fmt.Println("Failed to flush data to peer:", peer.ID, err)
+				stream.Close()
+				continue
+			}
+
+			stream.Close()
+		}
+	}
 }
 
+// Validate the block and propagate it to the network
 func propagateBlock(rw *bufio.ReadWriter, strm network.Stream) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic in gossipExecute:", r)
+		}
+	}()
 
+	for {
+		line, err := rw.ReadString('\n')
+		if err != nil {
+			return
+		}
+
+		if line == "" || line == "\n" {
+			continue
+		}
+
+		var block Block
+		err = json.Unmarshal([]byte(line), &block)
+		if err != nil {
+			fmt.Println("Failed to parse JSON:", err)
+			continue
+		}
+
+		// Add the block to the blockchain database
+		BlockMutex.RLock()
+		_, exists := Blockchain[block.Block_hash]
+		BlockMutex.RUnlock()
+
+		if !exists {
+
+			err := validateBlock(block) // Block validation
+			if err != nil {
+				fmt.Println("Block Validation Failed:", err)
+				continue
+			}
+
+			removeTxns(block)   // Remove Transactions from Mempool & Update UTXO
+			go compressMerkle() // Compress the Merkle Tree of the previous block
+			go buildMerkle(block.Transactions)
+			Latest_Block = block.Block_hash
+
+			// Add the Block to the database
+			BlockMutex.Lock()
+			Blockchain[block.Block_hash] = block
+			BlockMutex.Unlock()
+		} else {
+			continue
+		}
+
+		fmt.Printf("\x1b[32m> New Block Added to Blockchain\n> Block_hash: %s\n> Sent from %s\x1b[0m\n", block.Block_hash, strm.Conn().RemotePeer())
+
+		peerMutex.RLock()
+		peers := peerArray
+		peerMutex.RUnlock()
+
+		for _, peer := range peers {
+			if peer.ID == strm.Conn().RemotePeer() {
+				continue
+			}
+
+			stream, err := User.NewStream(context.Background(), peer.ID, protocol.ID("/blockchain/1.0.0/broadcast/block"))
+			if err != nil {
+				continue
+			}
+
+			rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+			data, err := json.Marshal(block)
+			if err != nil {
+				fmt.Println("Failed to serialize transaction:", err)
+				stream.Close()
+				continue
+			}
+
+			_, err = rw.WriteString(string(data) + "\n")
+			if err != nil {
+				fmt.Println("Failed to send transaction to peer:", peer.ID, err)
+				stream.Close()
+				continue
+			}
+
+			err = rw.Flush()
+			if err != nil {
+				fmt.Println("Failed to flush data to peer:", peer.ID, err)
+				stream.Close()
+				continue
+			}
+
+			stream.Close()
+		}
+	}
 }
