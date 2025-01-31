d0c8de2363b5684040a0d14ad9a3e4c82ffc3b70
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-01-31 14:53:13 +0530

    File exchange

--- Node/handlers.go
+++ Node/handlers.go
@@ -1,25 +1,51 @@
 package main
 
 import (
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/protocol"
+	"bufio"
+
+	"github.com/libp2p/go-libp2p/core/network"
 )
 
-func SetNodeHandlers(host host.Host, config Config) {
-	// Message handlers
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/message"), messageProtocol)
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/gossip"), broadcastMessage)
+// Messaging Stream Handlers
+func messageProtocol(stream network.Stream) {
 
-	// Propagation handlers
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/broadcast/transaction"), broadcastTxn)
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/braodcast/block"), broadcastBlock)
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
 
-	// Recieve handlers
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/recieve/block"), blockProtocol)
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/recieve/transaction"), txnProtocol)
+	go readData(rw)
+	// go writeData(rw)
+}
 
-	// Download handlers
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/download/blockchain"), downloadProtocol)
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/download/block"), downloadBlock)
-	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/download/transaction"), downloadTxn)
+// Propagation handlers
+func broadcastMessage(stream network.Stream) {
+
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+	go propagateMessage(rw, stream)
+
+}
+
+func broadcastTxn(stream network.Stream) {
+
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+	go propagateTxn(rw, stream)
+
+}
+
+func broadcastBlock(stream network.Stream) {
+
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+	go propagateBlock(rw, stream)
+
+}
+
+// Export Request handlers
+func exportBlockchain(stream network.Stream) {
+
+}
+
+func exportBlock(stream network.Stream) {
+
+}
+
+func exportTransaction(stream network.Stream) {
+
 }
--- Node/streams.go
+++ Node/streams.go
@@ -1,60 +1,21 @@
 package main
 
 import (
-	"bufio"
-
-	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/protocol"
 )
 
-// Propagation handlers
-func broadcastMessage(stream network.Stream) {
+func SetNodeHandlers(host host.Host, config Config) {
+	// Message handlers
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/message"), messageProtocol)
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/gossip"), broadcastMessage)
 
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-	go propagateMessage(rw, stream)
+	// Propagation handlers
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/broadcast/transaction"), broadcastTxn)
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/broadcast/block"), broadcastBlock)
 
-}
-
-func broadcastTxn(stream network.Stream) {
-
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-	go propagateTxn(rw, stream)
-
-}
-
-func broadcastBlock(stream network.Stream) {
-
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-	go propagateBlock(rw, stream)
-
-}
-
-// Messaging Stream Handlers
-func messageProtocol(stream network.Stream) {
-
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-
-	go readData(rw)
-	// go writeData(rw)
-}
-
-// Download Request handlers
-func downloadProtocol(stream network.Stream) {
-
-}
-
-func downloadBlock(stream network.Stream) {
-
-}
-
-func downloadTxn(stream network.Stream) {
-
-}
-
-// Reciever Stream handlers
-func blockProtocol(stream network.Stream) {
-
-}
-
-func txnProtocol(stream network.Stream) {
-
+	// Download handlers
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/download/blockchain"), exportBlockchain)
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/download/block"), exportBlock)
+	host.SetStreamHandler(protocol.ID(config.ProtocolID+"/download/transaction"), exportTransaction)
 }
